86778c8ff47f93ca7532bcf4012c49cf0bb1ab00
milkywaynian <marina.faraji1@gmail.com>
2020-05-17 18:10:01 +0100

    refactor stringReversal function

--- string-reversal.js
+++ string-reversal.js
@@ -1,29 +1,34 @@
-const stringReversal = (string) => {
-  //Step 1: Break down the string into an array of words
-  let newString = string.split(" ");
+//Step 1: Break down the string into an array of words
+const sentenceToWords = (sentence) => {
+    return sentence.split(" ");
+  } 
 
+//Step 2: reverse a word, split word in to an array of letters,reverse an array of letters, join the new array of letters into a word. 
+const wordReversal = (word) => {
+    const reversedWord = word
+    .split("")
+    .reverse()
+    .join("")
 
-  //Step 2: reverse a word
-  const wordReversal = (word) => reversedWord = word
-        //split word in to an array of letters
-        .split("")
-        //reverse an array of letters
-        .reverse()
-        //join the new array of letters into a word
-        .join("");
+  return reversedWord; 
+}
 
+//Step 3: reverse all words in a sentence 
 
-  //Step 3: reverse each word in a new string
+const reversedSentence = (string) => {
+    //string to words
+    let newString = sentenceToWords(string)
+    // reverse each word in a an array of words
+    newString = newString.map(newWord => wordReversal(newWord));
+    // join the words into sentence
+    newString = newString.join(" "); 
 
-  newString = newString.map(newWord => wordReversal(newWord));
+    return newString; 
 
-  // join reversed words into a sentence
-
-  newString = newString.join(" "); 
-
-  return newString; 
-  
 }
   
-  
-module.exports = stringReversal; 
+module.exports = {
+    sentenceToWords,
+    wordReversal,
+    reversedSentence
+}
--- string-reversal.test.js
+++ string-reversal.test.js
@@ -1,5 +1,13 @@
 const stringReversal = require('./string-reversal');
 
-test('revereses letters in words of a string', () => {
-    expect(stringReversal("cat and dog")).toBe("tac dna god");
+test('splits a sentence into an array of words', () => {
+    expect(stringReversal.sentenceToWords("cat and dog")).toEqual(["cat", "and", "dog"]);
 });
+
+test('reverses letters in a word', () => {
+    expect(stringReversal.wordReversal("cat")).toEqual("tac");
+});
+
+test('revereses letters in words of a string', () => {
+    expect(stringReversal.reversedSentence("cat and dog")).toEqual("tac dna god");
+})
