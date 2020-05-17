092d49f9893042cacd1fab2c5b612a0b26f92039
milkywaynian <marina.faraji1@gmail.com>
2020-05-17 18:02:25 +0100

    write a stringReversal function to pass a test

--- string-reversal.js
+++ string-reversal.js
@@ -1,5 +1,27 @@
 const stringReversal = (string) => {
-   
+  //Step 1: Break down the string into an array of words
+  let newString = string.split(" ");
+
+
+  //Step 2: reverse a word
+  const wordReversal = (word) => reversedWord = word
+        //split word in to an array of letters
+        .split("")
+        //reverse an array of letters
+        .reverse()
+        //join the new array of letters into a word
+        .join("");
+
+
+  //Step 3: reverse each word in a new string
+
+  newString = newString.map(newWord => wordReversal(newWord));
+
+  // join reversed words into a sentence
+
+  newString = newString.join(" "); 
+
+  return newString; 
   
 }
   
